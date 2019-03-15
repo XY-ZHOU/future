1febb4c2653e4d4e9f4b26d2af1adb7aa7edb3f8
XY-ZHOU <731986251@qq.com>
2019-03-15 10:40:18 +0800

    add class of user

--- src/main/java/company/model/User.java
+++ src/main/java/company/model/User.java
@@ -1,4 +1,27 @@
 package main.java.company.model;
 
 public class User {
+    private String name;
+    private String userId;
+
+    public User(String userId, String name) {
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
